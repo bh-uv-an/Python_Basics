b25afb2b8e4c8bb609ed9fd198d7973fe63bf1ea
Bhuvan Shetty <98102293+bh-uv-an@users.noreply.github.com>
2024-08-31 14:03:41 +0530

    Add files via upload

---
+++ Function Basics/Retu_mul_val_func.py
@@ -0,0 +1,12 @@
+def add(a,b):
+    return(a+b,a,b)
+
+a,b,c = add(11,22)
+
+print(a,b,c)
+
+'''
+Output:
+PS D:\Gen AI -Full> & "d:/Gen AI -Full/venv/python.exe" "d:/Gen AI -Full/Retu_mul_val_func.py"
+33 11 22
+'''
