3b10661db08fc78305a9ccf3841460aaa09edb9c
Bhuvan Shetty <98102293+bh-uv-an@users.noreply.github.com>
2024-09-01 19:00:52 +0530

    Create Time.py

---
+++ Python libraries/Time.py
@@ -0,0 +1,12 @@
+import time as t
+
+print("Present time = ",t.time())
+t.sleep(5)
+print("Present time = ",t.time())
+
+'''
+Output:
+PS D:\Gen AI -Full> & "d:/Gen AI -Full/venv/python.exe" "d:/Gen AI -Full/Time.py"
+Present time =  1725197317.968659
+Present time =  1725197322.9690409
+'''
